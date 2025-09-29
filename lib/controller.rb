a6dcd4bbb508391cdcf80363895f3cad28275768
FlowTech <admin@FlowTech-LAB.org>
2025-09-29 19:19:52 +0200

    bug author

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,7 +2,7 @@ require 'gossip'
 
 class ApplicationController < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
 
   get '/gossips/new/' do
@@ -11,8 +11,20 @@ class ApplicationController < Sinatra::Base
   
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
-      puts "Ceci est le contenu du hash params : #{params}"
-      puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
-      puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+    redirect '/'
+  end
+
+  get '/gossips/:id' do
+    erb :gossipx
+  end
+  get '/gossips/:id/edit' do
+    erb :edit
+  end
+    post '/gossips/:id/edit' do
+      id = params['id'].to_i
+      author = params['gossip_author']
+      content = params["gossip_content"]
+    Gossip.update(id, author, content)
+    redirect '/'
   end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,6 +1,10 @@
 require 'csv'
 
 class Gossip
+  attr_accessor :author, :content
+
+  @@all_gossips = []
+
   def initialize(author, content)
     @author = author
     @content = content
@@ -8,6 +12,32 @@ class Gossip
   def save
     CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
+    end
+  end
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+  def self.find(id)
+    idx = id - 1
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    gossipx = all_gossips[idx] #retourner le gossip
+    return gossipx
+  end
+  def self.update(id, author_update, content_update)
+    gossip_update = @@all_gossips[id]
+    gossip_update.author = author_update
+    gossip_update.content = content_update
+    CSV.open("./db/gossip.csv", "wb") do |csv| 
+      @@all_gossips.each do |gossip|     # Écrire chaque potin
+        csv << [gossip.author, gossip.content, gossip.comments.join("|")]
+      end
     end
   end
 end
