6d1fc917853cae4e6ac690f4a734dc3c8c2a856e
FlowTech <admin@FlowTech-LAB.org>
2025-09-29 12:19:30 +0200

    first commit

---
+++ lib/controller.rb
@@ -0,0 +1,18 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+  
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+      puts "Ceci est le contenu du hash params : #{params}"
+      puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+      puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+  end
+end
---
+++ lib/gossip.rb
@@ -0,0 +1,13 @@
+require 'csv'
+
+class Gossip
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+end
